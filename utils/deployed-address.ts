446e4da65317a73034205edce01983ac68d8db4d
squiver <damnariel555@gmail.com>
2024-08-09 14:25:03 +0700

    Add files via upload

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = ''
+export const ProxyAdmin = '0xC7160e4AD934664e4E35308939663E92FD54C6c1'
+export const SWTRImplementation = '0x7B2B578daaF446dB08A096c39DAa734A0743f42F'
+    
